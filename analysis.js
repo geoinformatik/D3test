225e12217363ab833015a34d19608d78e2f1d33d
Esbern Holmes <holmes@ruc.dk>
2018-04-15 18:36:54 +0200

    Update analysis.js

--- analysis.js
+++ analysis.js
@@ -1,1 +1,8 @@
-window.alert(5 + 6);
+d3.csv("cities.csv", function(data) {
+  data.forEach(function(d) {
+    d.population = +d.population;
+    d["land area"] = +d["land area"];
+  });
+  console.log(data[0]);
+  window.alert(5 + 6);
+});
