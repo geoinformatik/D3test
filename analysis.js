904df183936cc35f1ec89c2ae057b2bd3008dc6f
Esbern Holmes <holmes@ruc.dk>
2018-04-15 18:47:35 +0200

    Update analysis.js

--- analysis.js
+++ analysis.js
@@ -1,4 +1,4 @@
-d3.csv("cities.csv", function(data) {
+d3.csv("https://raw.githubusercontent.com/geoinformatik/D3test/master/cities.csv", function(data) {
   console.log(data[0]);
 });
 window.alert(5 + 6);
